66ae4e57d4d87ff1ca7b41b0eba91880375953c5
MaksimDan <damaksimovich@ucdavis.edu>
2015-12-17 19:46:16 -0800

    fixed bugs

--- js/customsearchengine.js
+++ js/customsearchengine.js
@@ -14,12 +14,12 @@ s.parentNode.insertBefore(gcse, s);
 
 $(document).on('click', function (e) {
 
-    if ($(e.target).closest('#bs-example-navbar-collapse-1 > ul.nav.navbar-nav.navbar-right > div').length) return false;
+    if ($(e.target).closest('#___gcse_0 > div').length) return false;
    
-	if ( $(e.target).closest('#___gcse_0 > div').length ) {
-    	$('#bs-example-navbar-collapse-1 > ul.nav.navbar-nav.navbar-right > div').toggle();
+	if ( $(e.target).closest('#bs-example-navbar-collapse-1 > ul.nav.navbar-nav.navbar-right > div > img').length ) {
+    	$('#___gcse_0 > div').toggle();
     } else {
-    	$('#bs-example-navbar-collapse-1 > ul.nav.navbar-nav.navbar-right > div').hide();
+    	$('#___gcse_0 > div').hide();
     }
 });
 
