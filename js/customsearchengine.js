54bda0bf3a23f3b0b6c7069878143842eee97589
MaksimDan <damaksimovich@ucdavis.edu>
2015-12-21 02:13:50 -0800

    Fixed custom search, and broken links

--- js/customsearchengine.js
+++ js/customsearchengine.js
@@ -1,17 +1,4 @@
 
-//custom search engine
-(function() {
-var cx = '001578284417931012478:9782p9w_jdy';
-var gcse = document.createElement('script');
-gcse.type = 'text/javascript';
-gcse.async = true;
-gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-	'//cse.google.com/cse.js?cx=' + cx;
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(gcse, s);
-})();
-
-
 $(document).on('click', function (e) {
 
     if ($(e.target).closest('#___gcse_0 > div').length) return false;
